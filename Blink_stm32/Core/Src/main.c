a80a8b828f33e9b09c316824aafb1f1c71de420b
Dmytriy <61518737+Dmytro-Bez@users.noreply.github.com>
2022-08-31 20:32:43 +0300

    Added project files
    Added files on stm32-based indication organization

---
+++ Blink_stm32/Core/Src/main.c
@@ -0,0 +1,122 @@
+#include "main.h"
+#include "stdbool.h"
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void blink_standart_gpio(void);
+static void blink_flag_gpio(void);
+
+bool flag;
+unsigned long T;
+
+int main(void){
+	T = HAL_GetTick();
+	flag = 1;
+
+	HAL_Init();
+	SystemClock_Config();
+	MX_GPIO_Init();
+
+	while (1){
+//		blink_standart_gpio();
+//		blink_flag_gpio();
+	}
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+}
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+static void blink_standart_gpio(void){
+	HAL_GPIO_WritePin(GPIOC, Led_Pin, 0);
+	HAL_Delay(2000);
+
+	HAL_GPIO_WritePin(GPIOC, Led_Pin, 1);
+	HAL_Delay(2000);
+}
+
+static void blink_flag_gpio(void){
+	if(flag == 1){
+		if(HAL_GetTick() - T >= 2000){
+			flag = 0;
+			T = HAL_GetTick();
+		}
+	}
+	if(flag == 0){
+		if(HAL_GetTick() - T >= 2000){
+			flag = 1;
+			T = HAL_GetTick();
+		}
+	}
+	HAL_GPIO_WritePin(GPIOC, Led_Pin, flag);
+}
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+
+}
+#endif
